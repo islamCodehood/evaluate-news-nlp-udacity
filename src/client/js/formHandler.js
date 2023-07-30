7d52b587f145782397b4eaa542a357a0371fec51
islamCodehood <islam.sayed8@gmail.com>
2023-07-30 19:28:23 +0300

    remove console.log

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,27 +1,36 @@
 function handleSubmit(event) {
     event.preventDefault()
-
+    const results = document.getElementById('results');
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    console.log(formText)
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
     fetch('http://localhost:8080/data', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
-            },
+        },
         body: JSON.stringify({
             text: formText
-    })})
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.text
-    }).catch(function(err) {
-        console.log(err)
-        document.getElementById('results').innerHTML = err
-    });
+        })
+    })
+        .then(res => res.json())
+        .then((res) => {
+            console.log(res)
+            const { agreement, subjectivity, confidence, irony } = res;
+            const html = `
+        <div class="result">Agreement: ${agreement}</div>
+        <div class="result">Subjectivity: ${subjectivity}</div>
+        <div class="result">Confidence: ${confidence}</div>
+        <div class="result">Irony: ${irony}</div>
+        `
+        //reset results
+            results.innerHTML = ""
+            //add new content
+            results.innerHTML = html
+        }).catch(function (err) {
+            console.log(err)
+        });
 }
 
 export { handleSubmit }
